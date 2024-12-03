331ede7b5d5151add6ddb1ea652b6a323a0a10b8
rameshlakmal <rameshslakmal1999@gmail.com>
2024-12-03 17:50:59 +0530

    add zerostep as a fixture

---
+++ fixtures/fixtures.js
@@ -0,0 +1,9 @@
+// fixtures.js
+import { test as base } from '@playwright/test';
+import { aiFixture } from '@zerostep/playwright';
+
+const test = base.extend({
+  ...aiFixture(base),
+});
+
+export default test; // Export `test` directly as the default export
