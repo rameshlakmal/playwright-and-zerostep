252aabceb68e532f3f3284952f5cace7fa7fe6c8
rameshlakmal <rameshslakmal1999@gmail.com>
2024-12-05 09:48:46 +0530

    update test script

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -9,7 +9,6 @@ test.describe('Zerostep and Playwright intigration demo' , () => {
 
 
   test.beforeEach(async ({page,ai}) => {
-    test.setTimeout(60000)
     await page.goto('https://www.saucedemo.com/')
     await ai ('Enter Username as "standard_user"')
     await ai ('Enter Password as "secret_sauce"')
@@ -19,11 +18,9 @@ test.describe('Zerostep and Playwright intigration demo' , () => {
 
   test('Verify that user can purchase a product successfully', async ({ ai,page,checkoutPage }) => {
 
-    // await ai ('Expand dropdown below the cart icon','Select "Price (low to high)" option')
     await ai ('Click "Add to cart" button in "Sauce Labs Backpack" product link')
     await ai ('Click "Add to cart" button in "Sauce Labs Bike Light" product link')
 
-//Assertions
     expect(page.locator('#remove-sauce-labs-backpack')).toContainText("Remove");
     expect(page.locator('#remove-sauce-labs-bike-light')).toContainText("Remove");
     expect(page.locator('.shopping_cart_badge')).toContainText("2");
@@ -32,22 +29,6 @@ test.describe('Zerostep and Playwright intigration demo' , () => {
     await ai("Click 'Checkout' button")
     await ai("Fill checkout form with realistic values")
     await ai("Click 'Continue' button")
-
-    await page.waitForURL('https://www.saucedemo.com/checkout-step-two.html');
-
-
-    await checkoutPage.assertTotalWithTax("Total: $43.18")
-
-
-
-    // expect(page.locator('.summary_total_label')).toContainText("Total: $43.18");
-    // await expect(page.locator('//*[@id="checkout_summary_container"]/div/div[2]/div[8]')).toContainText("Total: $43.18");
-
-
-
-    // const totalLabel = await page.locator('.summary_total_label').textContent();
-    // expect(totalLabel?.trim()).toBe('Total: $43.18');
-
     await ai("Click 'Finish' button")
 
     await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html")
